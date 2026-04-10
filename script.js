7d189d9d3e7dab89960178ce85c5fbcbc598f28e
Bruno <brunogminitti@gmail.com>
2026-04-09 23:18:49 -0300

    iniciando js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const hoje = new Date();
 
 document.getElementById("data-atual").innerText = hoje.toLocaleDateString("pt-BR")
+
+const descricao = document.getElementById("descricao");
+const valor = document.getElementById("valor");
+const tipo = document.getElementById("tipo");
+const categoria = document.getElementById("categoria");
+const data = document.getElementById("data");
+const botao = document.getElementById("adicionar");
+
+let transacoes = [];
+
+document.getElementById("adicionar").addEventListener("click", function(){
+    const descricao = document.getElementById("descricao").value;
+    const valor = document.getElementById("valor").value;
+    const tipo = document.getElementById("tipo").value;
+    const categoria = document.getElementById("categoria").value;
+    const data = document.getElementById("data").value;
+
+    const transacao = {
+        descricao: descricao,
+        valor: Number(valor),
+        tipo: tipo,
+        categoria: categoria,
+        data: data,
+    };
+    transacoes.push(transacao);
+
+    console.log(transacoes);
+});
