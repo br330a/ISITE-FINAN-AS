8a5d2ceab7d64d10ecb51255149d5e28d72244d2
Bruno <brunogminitti@gmail.com>
2026-05-16 17:46:29 -0300

    feat: histórico: dinâmica de lixeira e cores na tipagem

--- src/js/historico.js
+++ src/js/historico.js
@@ -11,19 +11,36 @@ function carregarDados() {
 
 function atualizarTabela() {
     historicoBody.innerHTML = "";
-    transacoes.forEach(function(transacao){
+    transacoes.forEach(function(transacao, index){
 
         historicoBody.innerHTML += `
             <tr>
                 <td>${transacao.descricao}</td>
                 <td>R$ ${transacao.valor.toFixed(2)}</td>
-                <td>${transacao.tipo}</td>
+                <td class="${transacao.tipo}">${transacao.tipo}</td>
                 <td>${transacao.categoria}</td>
                 <td>${transacao.data}</td>
+
+                <td>
+                    <button class="btnExcluir" onclick="deletarTransacao(${index})">
+                        🗑️
+                    </button>
+                </td>
             </tr>
         `;
     });
 }
 
+
 carregarDados();
 atualizarTabela();
+
+function deletarTransacao(index) {
+    transacoes.splice(index, 1);
+
+    localStorage.setItem(
+        "transacoes", 
+        JSON.stringify(transacoes)
+    );
+    atualizarTabela();
+}
