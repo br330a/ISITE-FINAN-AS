21a634d878f1b65cdf50701992877d0e232dded1
Bruno <brunogminitti@gmail.com>
2026-06-07 13:06:24 -0300

    sistema de login e validação funcionando e JWT (token)

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,6 @@
 const pool = require("../database/connection");
-
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function criarUsuario(req, res) {
 
@@ -33,7 +33,79 @@ async function criarUsuario(req, res) {
 }
 
 
+async function login(req, res) {
 
-module.exports = {
-    criarUsuario
+    const { email, senha } = req.body;
+
+    try {
+
+        const resultado = await pool.query(
+            `
+            SELECT *
+            FROM usuarios
+            WHERE email = $1
+            `,
+            [email]
+        );
+
+        if (resultado.rows.length === 0) {
+
+            return res.status(401).json({
+                erro: "Email ou senha inválidos"
+            });
+
+        }
+
+        const usuario = resultado.rows[0];
+
+        const senhaValida = await bcrypt.compare(  //função de comparação com correta e input
+            senha,
+            usuario.senha
+        );
+
+        if (!senhaValida) {
+
+            return res.status(401).json({
+                erro: "Email ou senha inválidos"
+            });
+
+        }
+
+        const token = jwt.sign(
+            {
+                id: usuario.id,
+                email: usuario.email
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        return res.status(200).json({
+            message: "Login realizado com sucesso",
+            token,
+            usuario: {
+                id: usuario.id,
+                nome: usuario.nome,
+                email: usuario.email
+            }
+        });
+
+    } catch (erro) {
+
+        console.error(erro);
+
+        return res.status(500).json({
+            erro: "Erro interno"
+        });
+
+    }
+
+}
+
+
+module.exports = { //rodando
+    criarUsuario,
+    login
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    criarUsuario
+    criarUsuario,
+    login
 } = require("../controllers/userController");
 
 router.post("/", criarUsuario);
+router.post("/login", login);
 
 module.exports = router;
